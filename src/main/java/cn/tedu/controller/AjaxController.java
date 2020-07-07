b74726c197c7623f97eb6fbf587bfe6d30e6031f
余浩 <980187223@qq.com>
2020-07-07 14:41:34 +0800

    ajax.html添加“doAjaxPost“接口

--- src/main/java/cn/tedu/controller/AjaxController.java
+++ src/main/java/cn/tedu/controller/AjaxController.java
@@ -13,4 +13,9 @@ public class AjaxController {
     public String doAjaxGetRequest(String key){
         return "query data by key="+key;
     }
+    @RequestMapping("doAjaxPostRequest")
+    @ResponseBody//将响应的内容以串的形式进行返回
+    public String doAjaxPostRequest(String key,String value){
+        return "save data ["+key+","+value+"]";
+    }
 }
